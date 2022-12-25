792162d1243ea4a9e42efd522fb6229ee7afb647
yehyaajour <medoyehya@hotmail.com>
2022-12-25 03:03:07 -0500

    firebase and notification

---
+++ lib/services/firebase_notification.dart
@@ -0,0 +1,138 @@
+import 'dart:async';
+import 'dart:developer';
+import 'dart:io';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'app_imports.dart';
+
+class NotificationHelper{
+  String token = '';
+   String navigationActionId = 'id_3';
+
+  final FirebaseMessaging firebaseMessaging = FirebaseMessaging.instance;
+  FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+  final StreamController<String?> selectNotificationStream = StreamController<String?>.broadcast();
+
+  void notificationTapBackground(NotificationResponse notificationResponse) {
+    // ignore: avoid_print
+    print('notification(${notificationResponse.id}) action tapped: '
+        '${notificationResponse.actionId} with'
+        ' payload: ${notificationResponse.payload}');
+    if (notificationResponse.input?.isNotEmpty ?? false) {
+      // ignore: avoid_print
+      print(
+          'notification action tapped with input: ${notificationResponse.input}');
+    }
+  }
+  initialNotification() async {
+    getToken();
+    var initializationSettingsAndroid = const AndroidInitializationSettings('@mipmap/app_icon');
+    var initializationSettingsIOS = const DarwinInitializationSettings();
+    var initializationSettings = InitializationSettings(
+        android: initializationSettingsAndroid,
+        iOS: initializationSettingsIOS);
+    flutterLocalNotificationsPlugin.initialize(
+      initializationSettings,
+      onDidReceiveNotificationResponse:
+          (NotificationResponse notificationResponse) {
+        switch (notificationResponse.notificationResponseType) {
+          case NotificationResponseType.selectedNotification:
+            selectNotificationStream.add(notificationResponse.payload);
+            break;
+          case NotificationResponseType.selectedNotificationAction:
+            if (notificationResponse.actionId == navigationActionId) {
+              selectNotificationStream.add(notificationResponse.payload);
+            }
+            break;
+        }
+      },
+      onDidReceiveBackgroundNotificationResponse: notificationTapBackground,
+    );
+
+    await firebaseMessaging.requestPermission(
+      alert: true,
+      announcement: false,
+      badge: true,
+      carPlay: false,
+      criticalAlert: false,
+      provisional: false,
+      sound: true,
+    );
+
+    await FirebaseMessaging.instance
+        .setForegroundNotificationPresentationOptions(
+      alert: true,
+      badge: true,
+      sound: true,
+    );
+
+    Future<void> firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+      print("Handling a background message");
+    }
+
+    // FirebaseMessaging.onBackgroundMessage(firebaseMessagingBackgroundHandler);
+
+    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) async {
+      if (message.notification != null) {
+        print('done FirebaseMessaging onMessageOpenedApp ');
+      }
+    });
+
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) async {
+      print('Got a message whilst in the foreground!');
+
+      RemoteNotification notification = message.notification!;
+      print(notification.title);
+      print(notification.body);
+
+
+      if (notification != null) {
+        print('done');
+        Platform.isIOS?print('done IOS'):showNotification(notification.title, notification.body);
+      }
+
+    });
+  }
+
+  Future onSelectNotification(String? payload) async {
+    if (payload != null) {
+      debugPrint('notification payload: $payload');
+    }
+  }
+
+  void showNotification(title, body) async {
+    await _demoNotification(
+      title,
+      body,
+    );
+  }
+
+  Future<void> _demoNotification(String title, String body) async {
+    var androidPlatformChannelSpecifics = const AndroidNotificationDetails(
+      'channel_ID',
+      'channel name',
+      channelDescription: 'channel description',
+      importance: Importance.max,
+      playSound: true,
+      showProgress: true,
+      priority: Priority.max,
+      ticker: 'test ticker',
+      icon: "@mipmap/ic_launcher"
+    );
+
+    var iOSChannelSpecifics = const DarwinNotificationDetails();
+    var platformChannelSpecifics = NotificationDetails(
+      android: androidPlatformChannelSpecifics,
+      iOS: iOSChannelSpecifics,
+    );
+    await flutterLocalNotificationsPlugin
+        .show(0, title, body, platformChannelSpecifics, payload: 'test',);
+  }
+
+  getToken() async {
+    firebaseMessaging.subscribeToTopic('all');
+    token = (await firebaseMessaging.getToken())!;
+    log('FcmToken $token');
+    return token;
+  }
+}
